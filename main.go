fd6cbcddad7521ffbf0fcd0f1d7ab9f6b46b8470
rithish279 <rajeswaranrithish@gmail.com>
2026-06-04 19:57:37 +0530

    feat: add background health checker with state-change logging

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync/atomic"
+	"time"
 )
 
 // enabling internal connection pool reusablilty across requests
@@ -27,11 +28,58 @@ func NewBackend(rawURL string) *Backend {
 	return b
 }
 
+func startHealthChecks(backends []*Backend, interval, timeout time.Duration) {
+	
+	client := &http.Client{
+		Timeout: timeout,
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse // No redirects in Health checks
+		},
+	}
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			for _, b := range backends {
+				checkHealth(b, client)
+			}
+		}
+	}()
+}
+
+func checkHealth(b *Backend, client *http.Client) {
+	url := b.URL.String() + "/"
+	resp, err := client.Get(url)
+
+	// Healthy only on clean 200 OK
+	isHealthy := err == nil && resp.StatusCode == http.StatusOK
+	if resp != nil {
+		resp.Body.Close()
+	}
+
+	// log only on transistions, not every tick
+	prev := b.Healthy.Swap(isHealthy)
+	if prev == isHealthy {
+		return
+	}
+
+	if isHealthy {
+		log.Printf("[health] backend %s is UP", b.URL)
+	} else {
+		log.Printf("[health] backend %s is DOWN", b.URL)
+	}
+}
+
 func main() {
 	backends := []*Backend{
 		NewBackend("http://localhost:9001"),
 		NewBackend("http://localhost:9002"),
 	}
+
+	// Health checks should start before traffic
+	startHealthChecks(backends, 5*time.Second, 2*time.Second)
 
 	var counter atomic.Uint32
 	
@@ -40,7 +88,7 @@ func main() {
 		backend := backends[idx]
 
 		if !backend.Healthy.Load() {
-			for i := 0; i <= len(backends); i++ {
+			for i := 0; i < len(backends); i++ {
 				idx = (idx + 1) % uint32(len(backends))
 				if backends[idx].Healthy.Load() {
 					backend = backends[idx]
